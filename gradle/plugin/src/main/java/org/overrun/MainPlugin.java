66f79678a4d19da46f5b4efd40702e3a2b0acd4f
baka4n <qq474899581@gmail.com>
2023-06-19 19:32:43 +0800

    暂时更新，gradle plugin持续研发中

--- gradle/plugin/src/main/java/org/overrun/MainPlugin.java
+++ gradle/plugin/src/main/java/org/overrun/MainPlugin.java
@@ -8,5 +8,7 @@ public class MainPlugin implements Plugin<Project> {
     @Override
     public void apply(@NotNull Project target) {
         System.out.printf("load plugin version%s%n\n", target.getVersion());
+        target.getExtensions().add("win32", Win32.class);
+//        Win32 path = (Win32) target.getExtensions().getByName("path");
     }
 }
---
+++ gradle/plugin/src/main/java/org/overrun/Win32.java
@@ -0,0 +1,7 @@
+package org.overrun;
+
+import java.nio.file.Path;
+
+public interface Win32 {
+    Path getPath();
+}
